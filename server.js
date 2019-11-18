0816b82c6326fd4346bda936ef51368984a9033f
locdangxuan <xuanloc20041998@gmail.com>
2019-11-18 15:36:19 +0700

    set environemt variable to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 //<---------------const module--------------------->
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //<---------------const api route------------------->
 const mongoose = require('./config/database'); //database configuration
